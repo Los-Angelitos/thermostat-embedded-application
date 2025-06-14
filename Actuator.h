48f1ba4b0ee558e24433367942cafbcb02e72ffd
Arian Rodriguez <arianmrv12@gmail.com>
2025-06-14 12:56:01 -0500

    feat: added actuator header for implementation

---
+++ Actuator.h
@@ -0,0 +1,28 @@
+#ifndef ACTUATOR_H
+#include "CommandHandler.h"
+
+/**
+ * @file Actuator.h
+ * @brief This file defines the Actuator class, which represents an actuator that can handle commands.
+ * * The Actuator class inherits from CommandHandler and implements the handle method to process commands.
+ * * The Actuator class can be associated with a GPIO pin and can delegate command handling to another CommandHandler.
+ * * Usage:
+ * To create an actuator, instantiate the Actuator class with a GPIO pin number and optionally a command handler.
+ * The handle method will be called with a Command object when a command is issued.
+ * 
+ * @author Arian Rodriguez
+ * @date June 13, 2025
+ * @version 1.0
+ */
+
+class Actuator : public CommandHandler {
+protected:
+    int pin; // GPIO pin number for the actuator
+    CommandHandler* commandHandler; // Pointer to the command handler
+public:
+    Actuator(int pin, CommandHandler* commandHandler = nullptr);
+    void handle(Command command) override;
+    void setHandler(CommandHandler* handler);
+};
+
+#endif // ACTUATOR_H
