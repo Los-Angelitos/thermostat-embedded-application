24b7ac484925814b37741fe2d9566a8317bb2801
Arian Rodriguez <arianmrv12@gmail.com>
2025-06-14 12:56:57 -0500

    feat: added implementation for Device header

---
+++ Device.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file Device.cpp
+ * @brief This file implements the Device class, which represents a device that can handle events and commands.
+ * 
+ * Placeholder for Device class implementation.
+ * 
+ * @author Arian Rodriguez
+ * @date June 13, 2025
+ * @version 1.0
+ */
+
+#include "Device.h"
+
+// No implementation needed for the Device class as it is an abstract base class.
